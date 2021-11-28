f9caeae6bb7e3955dc0fb79f86c0193ce7839fee
Tri Bui <buitri91@gmail.com>
2021-11-28 12:08:25 -0800

    Add comments to make_prediction in  gb_model/predict.py

--- packages/gb_model/gb_model/predict.py
+++ packages/gb_model/gb_model/predict.py
@@ -21,34 +21,41 @@ def make_prediction(meter_data, weather_data, time_var='timestamp'):
     :return: (dictionary) predictions in JSON format
     """
 
+    # Read and validate data
     building = pd.read_pickle(config.DATA_PATH / 'building.pkl')
     meter = pd.DataFrame(meter_data)
     weather = pd.DataFrame(weather_data)
-
     data_validation.validate_data(meter, weather,
                                   config.METER_COLS, config.WEATHER_COLS)
 
+    # Convert timestamps to dt
     meter[time_var] = pd.to_datetime(meter[time_var], unit='ms')
     weather[time_var] = pd.to_datetime(weather[time_var], unit='ms')
 
+    # Preprocess weather data
     start = weather[time_var].min()
     end = weather[time_var].max()
-
     weather = wpipe.fit_transform(weather,
                                   time_reindexer__t_start=start,
                                   time_reindexer__t_end=end)
 
+    # Merge data and select feats
     df = preprocessing.merge_data(meter, weather, building)
     df = fpipe.fit_transform(df)
 
+    # Check for missing vals
     assert df.isnull().sum().sum() == 0, 'Missing values detected.'
 
+    # Make preds
     pred = pipeline.pred_pipe(df,
                               config.MODEL_PATH / 'rare_enc',
                               config.MODEL_PATH / 'mean_enc',
                               config.MODEL_PATH / 'scaler',
-                              config.MODEL_PATH / 'lgb')
+                              config.MODEL_PATH / 'lgb', 
+                              use_model0=True, use_xgb=False, 
+                              inverse_transform_site0=False)
 
+    # Log - model version, input data, and preds
     logger.info(
         f'Model version: {__version__}'
         f'\nInput meter data: {meter}'
@@ -56,5 +63,6 @@ def make_prediction(meter_data, weather_data, time_var='timestamp'):
         f'\nPredictions: {pred}'
     )
 
+    # Format preds
     response = {'predictions': pred, 'version': __version__}
     return response
